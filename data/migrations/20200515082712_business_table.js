6d6575adf0b716709aa6e3ee0d2da76e76a6d9d7
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-15 09:02:18 -0700

    create the tables of projects , resources , tasks and projects_resources

--- data/migrations/20200515082712_business_table.js
+++ data/migrations/20200515082712_business_table.js
@@ -1,8 +1,64 @@
-
 exports.up = async function(knex) {
-    
+  await knex.schema.createTable("projects", project => {
+    project.increments("id");
+    project.string("name", 225).notNullable();
+    project.string("description");
+    project
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false);
+  });
+
+  await knex.schema.createTable("resources", resource => {
+    resource.increments("id");
+    resource.string("name", 225).notNullable();
+    resource.string("description");
+  });
+
+  await knex.schema.createTable("tasks", task => {
+    task.increments("id");
+    task.string("description").notNullable();
+    task.string("notes");
+    task
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false);
+    task
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+
+  await knex.schema.createTable("projects_resources", projectResource => {
+    projectResource
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    projectResource
+      .integer("resource_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("resources")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    projectResource.primary(["project_id", "resource_id"]);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("projects_resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
 };

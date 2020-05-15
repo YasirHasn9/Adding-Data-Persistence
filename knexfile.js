0817ee56f7e783cb31ee7ab092ca3bf0e756bc62
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-15 08:28:05 -0700

    create migrations

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: "./data/zoos.db3"
+      filename: "./data/business.db3"
     },
     migrations: {
       directory: "./data/migrations"

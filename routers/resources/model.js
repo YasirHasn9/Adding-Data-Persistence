13ffe6279ce864edc64970dcafa5d304e69ef1b4
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-15 10:06:04 -0700

    create new resources post /projects/resources

--- routers/resources/model.js
+++ routers/resources/model.js
@@ -8,4 +8,9 @@ function find() {
   return db("resources");
 }
 
-
+async function add(project) {
+  const [id] = await db("resources").insert(project);
+  return db("resources")
+    .where({ id })
+    .first();
+}

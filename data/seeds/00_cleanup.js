5e0c14235e4881e2b5e1824e65ba225a5c7f6d9e
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-15 09:12:21 -0700

    create up a seed file for cleanup for the 4 tables

---
+++ data/seeds/00_cleanup.js
@@ -0,0 +1,7 @@
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex("projects_resources").truncate();
+  await knex("tasks").truncate();
+  await knex("resources").truncate();
+  await knex("projects").truncate();
+};
